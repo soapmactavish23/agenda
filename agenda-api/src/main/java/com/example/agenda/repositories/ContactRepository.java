add16a5be269657a1717ec91da5fc6680883baaf
soapmactavish23 <ricknogueira1231@gmail.com>
2026-07-15 22:20:20 -0300

    Repositories ok

---
+++ agenda-api/src/main/java/com/example/agenda/repositories/ContactRepository.java
@@ -0,0 +1,7 @@
+package com.example.agenda.repositories;
+
+import com.example.agenda.models.Contact;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ContactRepository extends JpaRepository<Contact, Integer> {
+}
