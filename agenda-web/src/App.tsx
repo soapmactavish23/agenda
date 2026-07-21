8cad308be6d5f63059017ece09530227fecfb3e7
soapmactavish23 <ricknogueira1231@gmail.com>
2026-07-20 21:05:22 -0300

    Delete Ok

--- agenda-web/src/App.tsx
+++ agenda-web/src/App.tsx
@@ -11,6 +11,7 @@ import { Fieldset } from "primereact/fieldset";
 import type { Contact } from "./types/contact";
 import { contactService } from "./services/contactService";
 import { useQuery } from "@tanstack/react-query";
+import { confirmDialog, ConfirmDialog } from "primereact/confirmdialog";
 export default function App() {
   const dt = useRef<DataTable<Contact[]>>(null);
 
@@ -26,8 +27,23 @@ export default function App() {
     },
   });
 
+  const handleDelete = (rowData: Contact) => {
+    confirmDialog({
+      header: "Confirmar",
+      message: `Tem certeza que deseja deletar o registro #${rowData.name}?`,
+      icon: "pi pi-info-circle",
+      acceptLabel: "Confirmar",
+      rejectLabel: "Cancelar",
+      accept: async () => {
+        await contactService.delete(rowData.id!);
+        refetch();
+      },
+    });
+  };
+
   return (
     <div className="p-8">
+      <ConfirmDialog />
       <Fieldset legend="Agenda">
         <DataTable
           ref={dt}
@@ -65,11 +81,27 @@ export default function App() {
           <Column field="contact" header="Contato"></Column>
           <Column field="email" header="E-mail"></Column>
           <Column
-            body={
-              <>
-                <h1>teste</h1>
-              </>
-            }
+            body={(rowData: Contact) => {
+              return (
+                <>
+                  <Button
+                    icon="pi pi-pencil"
+                    rounded
+                    severity="success"
+                    className="mr-2"
+                    onClick={() => {
+                      console.log("Editar");
+                    }}
+                  />
+                  <Button
+                    icon="pi pi-trash"
+                    rounded
+                    severity="danger"
+                    onClick={() => handleDelete(rowData)}
+                  />
+                </>
+              );
+            }}
           ></Column>
         </DataTable>
       </Fieldset>
